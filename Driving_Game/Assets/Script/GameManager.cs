1623bc3ee8a161e93b5192e3e285004cef4a9da1
Akashps2026 <akashps078@gmail.com>
2025-11-01 19:43:49 +0530

    Drive 1st Push

---
+++ Driving_Game/Assets/Script/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GroundPrefab;
+    public float SpawnDistance = 120f;
+    public Transform PlayerTransform;
+    public GameObject gameOverPanel;
+
+    private Vector3 nextSpawnPosition;
+
+    void Start()
+    {
+        nextSpawnPosition = Vector3.zero;
+        SpawnGround();
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(false);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0f; // Pause the game
+
+        if (gameOverPanel != null)
+            gameOverPanel.SetActive(true);
+    }
+
+    public void RestartGame()
+    {
+        // Unpause before reloading the scene
+        Time.timeScale = 1f;
+
+        // Reload current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    void Update()
+    {
+        if (PlayerTransform.position.z + SpawnDistance > nextSpawnPosition.z)
+            SpawnGround();
+    }
+
+    void SpawnGround()
+    {
+        Instantiate(GroundPrefab, nextSpawnPosition, Quaternion.identity);
+        nextSpawnPosition.z += 120f;
+    }
+}
---
+++ Driving_Game/Assets/Script/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Move;
+    public float Horizontal;
+    public float Vertical;
+    private float speed=20f;
+    public  Rigidbody rb;
+
+    public AudioSource hitSound; 
+    public AudioSource Sound;
+    private bool hasHit = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        Vector3 Move=new Vector3(Horizontal,0,Vertical);
+        transform.Translate(Move*speed*Time.deltaTime);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!hasHit && collision.gameObject.CompareTag("Obstacle"))
+        {
+            hasHit = true;
+
+            // 🔊 Play hit sound
+            if (hitSound != null)
+            {
+                hitSound.Play();
+            }
+
+            // 🕒 Wait a short delay before showing Game Over (so sound can play)
+            StartCoroutine(DelayGameOver());
+        }
+    }
+
+    private System.Collections.IEnumerator DelayGameOver()
+    {
+        yield return new WaitForSeconds(0.2f); // wait 0.4 seconds for sound
+        FindObjectOfType<GameManager>().GameOver();
+    }
+}
